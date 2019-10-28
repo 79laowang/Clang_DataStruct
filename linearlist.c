1ac4b3849784a5682a3a0bc24e2d368d24a583d4
Ke <edward.wang@163.com>
2019-10-28 23:27:53 +0800

    init repository

---
+++ linearlist.c
@@ -0,0 +1,17 @@
+/*------------------------------------------------------------------------------
+* File Name:   linearlist.c
+* Purpose:     Linear list implementation
+*
+* Author:      Ke Wang
+*
+* Created:     2019-10-28
+* Copyright:   (c) Ke Wang 2019
+* Licence:     <your licence>
+*-----------------------------------------------------------------------------*/
+
+#include <stdio.h>
+
+int main()
+{
+    return (0);
+}

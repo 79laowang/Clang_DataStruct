41f1357681c051b2035240d81375ee522c80e178
79laowang <edward.wang@163.com>
2019-11-08 10:49:02 +0800

    Add some comments

--- bubble-sort.c
+++ bubble-sort.c
@@ -1,6 +1,6 @@
 /*------------------------------------------------------------------------------
 * File Name:   bubble-sort.c
-* Purpose:     冒泡排序 
+* Purpose:     冒泡排序,交换排序之一
 *
 * Author:      Ke Wang
 *
@@ -9,27 +9,32 @@
 * Licence:     <your licence>
 *-----------------------------------------------------------------------------*/
 
+/*
+Worst time complexity: O(n^2)
+Optimal time complexity: O(n)
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #define MAX 20
 
 /* 冒泡排序法 */
-void bubble(char *arr,int count)
+void bubble(char *arr, int n)
 {
     int i,j;
     char temp;
-    for(j=count;j>1;j--) {
-        int count = 0;
-        for(i=0;i<j-1;i++)
-            if(arr[i+1]<arr[i])
-            {
-                temp=arr[i+1];
-                arr[i+1]=arr[i];
-                arr[i]=temp;
-                count++;
+    for (j= n; j > 0; j--) {
+        int exchange = 0;
+        for (i = 0; i < j-1; i++)
+            if(arr[i+1] < arr[i]) { 
+                temp = arr[i+1];
+                arr[i+1] = arr[i];
+                arr[i] = temp;
+                exchange = 1;
             }
-        if (count) break;
+        /* 如果没有发生交换，退出进入下一轮比较 */
+        if (exchange == 0) break;
     }
     printf("Sorted output:[%s]\n",arr);
 }
@@ -47,13 +52,12 @@ int main()
 		i = 0;
 	    while(array[i]!='\n' && array[i]!='\0')
 			i++;
-		if(array[i]=='\n')
-			array[i]='\0';
+		if(array[i]=='\n') array[i]='\0';
 		else
 			while(getchar()!='\n')
 				continue;
         count=strlen(array);
-        bubble(array,count);
+        bubble(array, count);
 	}
     return (0);
 }

0651316a097c4a61089446ec66b7f6fe48d66071
79laowang <edward.wang@163.com>
2019-12-02 23:38:04 +0800

    link list operations

---
+++ linklist.c
@@ -0,0 +1,19 @@
+/*------------------------------------------------------------------------------
+* File Name:   linklist.c
+* Purpose:     link list operations 
+*
+* Author:      Ke Wang
+*
+* Created:     2019-12-02
+* Copyright:   (c) Ke Wang 2019
+* Licence:     <your licence>
+*-----------------------------------------------------------------------------*/
+
+#include "common.h"
+
+Node head;
+
+int insert(int val)
+{
+    Node p, q;
+}
